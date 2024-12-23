aff708c5557ee8993fcb5c63997cec2d2df882a0
AdarshFiveonline <helpdesk@fiveonline.in>
2024-12-23 13:47:06 +0530

    added new sections

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -71,7 +71,7 @@ const Home = () => {
             <div className="banner-text">
               <h1 className="banner-title">A Promising future begins here!</h1>
 
-              <p>
+              <p className="paragraph">
                 We promote equality and curiosity mindsets through direct and
                 indirect assistance in education, for children seeking quality
                 teaching and personal growth.
@@ -231,11 +231,13 @@ const Home = () => {
                       alt="vector-6"
                       className="bridging-img1"
                     />
-                    <img
-                      src="/images/banner/two-students-studying-together-online-with-laptop-park.png"
-                      alt="briding-img"
-                      className="bridging-img2"
-                    />
+                    <div className="bridging-img2">
+                      <img
+                        src="/images/banner/two-students-studying-together-online-with-laptop-park.png"
+                        alt="briding-img"
+                      />
+                    </div>
+
                     <img
                       src="/images/banner/Vector 7.png"
                       alt="vector-7"
@@ -251,7 +253,7 @@ const Home = () => {
                       Bridging Gaps with Accessible, Holistic Learning
                     </h2>
 
-                    <p>
+                    <p className="paragraph bridge-para">
                       Providing education that’s accessible to rural and
                       low-income communities and opening learning opportunities
                       that integrate top-tier education with holistic life
@@ -270,7 +272,351 @@ const Home = () => {
           </div>
         </section>
 
-        <section className="half-img-section"></section>
+        <section className="half-img-section">
+          <div className="container">
+            <div className="half-img-text">
+              <h2 className="section-title text-center">
+                Bringing new opportunities to the underdogs since 2019
+              </h2>
+
+              <button className="custom-btn bridge-btn read-btn">
+                <NavLink class="nav-link" to="/">
+                  READ OUR NEWSLETTER
+                </NavLink>
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <section className="support-section">
+          <div className="container">
+            <div className="support-text-div">
+              <h2>Your support is meaningful.</h2>
+              <div className="support-para">
+                <p className="paragraph bridge-para p-0">
+                  “Out of 100 students, 29 percent of girls and boys drop out of
+                  school before completing the full cycle of elementary
+                  education, and often they are the most marginalized children.”
+                </p>
+                <p className="paragraph bridge-para unicef">©UNICEF</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="testimonials">
+          <div className="container-fluid">
+            <div className="col-lg-12">
+              <div className="row align-items-center justify-content-center">
+                <div className="col-lg-10 order-lg-1 order-2">
+                  <div className="tab-content">
+                    <div
+                      className="tab-pane fade active show"
+                      id="member1"
+                      role="tabpanel"
+                      aria-labelledby="member1-tab"
+                    >
+                      <div className="col-lg-12">
+                        <div className="row align-items-center justify-content-center">
+                          <div className="testimonial-img1 col-lg-1">
+                            <img
+                              src="/images/testimonials/quote.png"
+                              alt="quote"
+                            />
+                          </div>
+
+                          <div className="testimonial-text col-lg-6 order-lg-1 order-2">
+                            <h2 className="section-title">
+                              Impactful Testimonials
+                            </h2>
+                            <p className="paragraph bridge-para testimonial-para">
+                              Lorem ipsum dolor sit amet. Rem distinctio
+                              corrupti cum earum iusto et veritatis facere ut
+                              tempora sapiente eum assumenda amet. Non deleniti
+                              porro est repudiandae aperiam ut fugit incidunt
+                              sed esse animi id voluptates laboriosam et sequi
+                              modi
+                            </p>
+
+                            <div className="testimonial-name ">
+                              <h6>Eric Francis</h6>
+                              <p>Volunteer</p>
+                            </div>
+                          </div>
+                          <div className="testimonial-img col-lg-5 order-lg-2 order-1">
+                            <img
+                              src="/images/testimonials/testi-1-copyright-120x120.png"
+                              alt="member"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div
+                      className="tab-pane fade"
+                      id="member2"
+                      role="tabpanel"
+                      aria-labelledby="member2-tab"
+                    >
+                      <div className="col-lg-12">
+                        <div className="row align-items-center justify-content-center">
+                          <div className="testimonial-img1 col-lg-1">
+                            <img
+                              src="/images/testimonials/quote.png"
+                              alt="quote"
+                            />
+                          </div>
+
+                          <div className="testimonial-text col-lg-6 order-lg-1 order-2">
+                            <h2 className="section-title">
+                              Impactful Testimonials
+                            </h2>
+                            <p className="paragraph bridge-para testimonial-para">
+                              Lorem ipsum dolor sit amet. Rem distinctio
+                              corrupti cum earum iusto et veritatis facere ut
+                              tempora sapiente eum assumenda amet. Non deleniti
+                              porro est repudiandae aperiam ut fugit incidunt
+                              sed esse animi id voluptates laboriosam et sequi
+                              modi
+                            </p>
+
+                            <div className="testimonial-name ">
+                              <h6>Eric Francis</h6>
+                              <p>Volunteer</p>
+                            </div>
+                          </div>
+                          <div className="testimonial-img col-lg-5 order-lg-2 order-1">
+                            <img
+                              src="/images/testimonials/testi-2-copyright-120x120.png"
+                              alt="member"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div
+                      className="tab-pane fade"
+                      id="member3"
+                      role="tabpanel"
+                      aria-labelledby="member3-tab"
+                    >
+                      <div className="col-lg-12">
+                        <div className="row align-items-center justify-content-center">
+                          <div className="testimonial-img1 col-lg-1">
+                            <img
+                              src="/images/testimonials/quote.png"
+                              alt="quote"
+                            />
+                          </div>
+
+                          <div className="testimonial-text col-lg-6 order-lg-1 order-2">
+                            <h2 className="section-title">
+                              Impactful Testimonials
+                            </h2>
+                            <p className="paragraph bridge-para testimonial-para">
+                              Lorem ipsum dolor sit amet. Rem distinctio
+                              corrupti cum earum iusto et veritatis facere ut
+                              tempora sapiente eum assumenda amet. Non deleniti
+                              porro est repudiandae aperiam ut fugit incidunt
+                              sed esse animi id voluptates laboriosam et sequi
+                              modi
+                            </p>
+
+                            <div className="testimonial-name ">
+                              <h6>Eric Francis</h6>
+                              <p>Volunteer</p>
+                            </div>
+                          </div>
+                          <div className="testimonial-img col-lg-5 order-lg-2 order-1">
+                            <img
+                              src="/images/testimonials/testi-3-copyright-120x120.png"
+                              alt="member"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div
+                      className="tab-pane fade"
+                      id="member4"
+                      role="tabpanel"
+                      aria-labelledby="member4-tab"
+                    >
+                      <div className="col-lg-12">
+                        <div className="row align-items-center justify-content-center">
+                          <div className="testimonial-img1 col-lg-1">
+                            <img
+                              src="/images/testimonials/quote.png"
+                              alt="quote"
+                            />
+                          </div>
+
+                          <div className="testimonial-text col-lg-6 order-lg-1 order-2">
+                            <h2 className="section-title">
+                              Impactful Testimonials
+                            </h2>
+                            <p className="paragraph bridge-para testimonial-para">
+                              Lorem ipsum dolor sit amet. Rem distinctio
+                              corrupti cum earum iusto et veritatis facere ut
+                              tempora sapiente eum assumenda amet. Non deleniti
+                              porro est repudiandae aperiam ut fugit incidunt
+                              sed esse animi id voluptates laboriosam et sequi
+                              modi
+                            </p>
+
+                            <div className="testimonial-name ">
+                              <h6>Eric Francis</h6>
+                              <p>Volunteer</p>
+                            </div>
+                          </div>
+                          <div className="testimonial-img col-lg-5 order-lg-2 order-1">
+                            <img
+                              src="/images/testimonials/testi-2-copyright-120x120.png"
+                              alt="member"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div
+                      className="tab-pane fade"
+                      id="member5"
+                      role="tabpanel"
+                      aria-labelledby="member5-tab"
+                    >
+                      <div className="col-lg-12">
+                        <div className="row align-items-center justify-content-center">
+                          <div className="testimonial-img1 col-lg-1">
+                            <img
+                              src="/images/testimonials/quote.png"
+                              alt="quote"
+                            />
+                          </div>
+
+                          <div className="testimonial-text col-lg-6 order-lg-1 order-2">
+                            <h2 className="section-title">
+                              Impactful Testimonials
+                            </h2>
+                            <p className="paragraph bridge-para testimonial-para">
+                              Lorem ipsum dolor sit amet. Rem distinctio
+                              corrupti cum earum iusto et veritatis facere ut
+                              tempora sapiente eum assumenda amet. Non deleniti
+                              porro est repudiandae aperiam ut fugit incidunt
+                              sed esse animi id voluptates laboriosam et sequi
+                              modi
+                            </p>
+
+                            <div className="testimonial-name ">
+                              <h6>Eric Francis</h6>
+                              <p>Volunteer</p>
+                            </div>
+                          </div>
+                          <div className="testimonial-img col-lg-5 order-lg-2 order-1">
+                            <img
+                              src="/images/testimonials/testi-2-copyright-120x120.png"
+                              alt="member"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-2 testimonial-links order-lg-2 order-1">
+                  <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                      <a
+                        class="nav-link active"
+                        aria-current="page"
+                        href="#member1"
+                        id="member1-tab"
+                        data-bs-toggle="tab"
+                        aria-controls="member1"
+                      >
+                        <img
+                          src="/images/testimonials/testi-1-copyright-120x120.png"
+                          alt="member"
+                        />
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a
+                        class="nav-link"
+                        href="#member2"
+                        id="member2-tab"
+                        data-bs-toggle="tab"
+                        aria-controls="member2"
+                      >
+                        <img
+                          src="/images/testimonials/testi-2-copyright-120x120.png"
+                          alt="member"
+                        />
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a
+                        class="nav-link"
+                        href="#member3"
+                        id="member3-tab"
+                        data-bs-toggle="tab"
+                        aria-controls="member3"
+                      >
+                        <img
+                          src="/images/testimonials/testi-3-copyright-120x120.png"
+                          alt="member"
+                        />
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a
+                        class="nav-link"
+                        href="#member4"
+                        id="member4-tab"
+                        data-bs-toggle="tab"
+                        aria-controls="member4"
+                      >
+                        <img
+                          src="/images/testimonials/testi-2-copyright-120x120.png"
+                          alt="member"
+                        />
+                      </a>
+                    </li>
+
+                    <li class="nav-item">
+                      <a
+                        class="nav-link"
+                        href="#member5"
+                        id="member5-tab"
+                        data-bs-toggle="tab"
+                        aria-controls="member5"
+                      >
+                        <img
+                          src="/images/testimonials/testi-2-copyright-120x120.png"
+                          alt="member"
+                        />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="news-events">
+          <div className="container">
+            <div className="col-lg-12">
+              <div className="row">
+                <div className="col-lg-6"></div>
+                <div className="col-lg-6"></div>
+              </div>
+            </div>
+          </div>
+        </section>
       </Layout>
     </div>
   );
