5342f5278171f6bebf747ac82ea57523408cd8f2
AdarshFiveonline <helpdesk@fiveonline.in>
2024-12-23 18:39:30 +0530

    done all changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "aos/dist/aos.css";
+import "animate.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
